12a2dd96ba882cac0dee1aca832e60aa0428a6b6
Front-end Engineer <cleber.programmer@hotmail.com>
2015-08-21 13:56:47 -0300

    Update apply.js

--- apply.js
+++ apply.js
@@ -11,7 +11,7 @@
  *        $apply($add, ['hello ', 'cleber.programmer']);
  * 
  */
-Ninja.service('$apply', ['$curry'], function ($curry) {
+Ninja.module('$apply', ['$curry'], function ($curry) {
   
   /**
    * Chama uma funcao determininado valores fornecido de uma array
